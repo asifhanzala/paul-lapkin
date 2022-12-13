436c8171d2813b2954b5b097d56d6f8b5462e570
hanzalawd <asifhanzala@gmail.com>
2022-12-13 19:32:51 +0600

    first commit

---
+++ resources/js/custom.js
@@ -0,0 +1,16 @@
+function openMenu () {
+    document.getElementById('navbar').style.width = "100%";
+}
+
+function closeMenu () {
+    document.getElementById('navbar').style.width = "0%";
+}
+
+$(function(){
+    $('.menu-item a, .next-section a').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 2000);
+        return false
+    });
+});
